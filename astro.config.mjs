e0deea132ee252e93723747deaa981920b22ea56
Vlad Gafarov <vlad.gafarov02@gmail.com>
2023-01-10 18:17:13 +0500

    add github action for deploing site

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://vladgafarov.github.io',
+	base: '/personal-site',
 	integrations: [
 		mdx(),
 		sitemap(),
