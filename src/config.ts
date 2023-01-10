1dddd29409723d2e3462d7c59f70a9d1e56b91fa
Vlad Gafarov <vlad.gafarov02@gmail.com>
2023-01-10 18:54:18 +0500

    add page titles

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,4 @@
 export const SITE_TITLE = 'My personal website'
+export const MAIN_PAGE_TITLE = 'Main page'
+export const BLOG_PAGE_TITLE = 'Blog page'
 export const SITE_DESCRIPTION = 'Welcome to my website!'
