191a7f45769cd2d051c52cc660aab2e72e723b61
Vlad Gafarov <vlad.gafarov02@gmail.com>
2023-01-10 22:13:14 +0500

    base skills section

---
+++ src/components/ShowSkillsFullList.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { useConfirmDialog } from '@vueuse/core'
+
+const { isRevealed, reveal, confirm, cancel, onReveal, onConfirm, onCancel } =
+	useConfirmDialog()
+</script>
+
+<template>
+	<button @click="reveal" class="text-dark dark:text-white underline">
+		Show me full list
+	</button>
+
+	<teleport to="body">
+		<div v-if="isRevealed" class="modal-bg">
+			<div class="modal">
+				<h2>Confirm?</h2>
+				<button @click="confirm">Yes</button>
+				<button @click="cancel">Cancel</button>
+			</div>
+		</div>
+	</teleport>
+</template>
