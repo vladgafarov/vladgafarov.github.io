c7f4b9520e1b53e7371a4d3bc115b640a47a3592
Vlad Gafarov <vlad.gafarov02@gmail.com>
2023-01-11 11:36:16 +0500

    create full list of skills modal

--- src/components/ShowSkillsFullList.vue
+++ src/components/ShowSkillsFullList.vue
@@ -1,22 +1,76 @@
 <script setup lang="ts">
-import { useConfirmDialog } from '@vueuse/core'
+import {
+	Dialog,
+	DialogPanel,
+	DialogTitle,
+	TransitionChild,
+	TransitionRoot,
+} from '@headlessui/vue'
+import { XMarkIcon } from '@heroicons/vue/24/outline'
+import { ref } from 'vue'
 
-const { isRevealed, reveal, confirm, cancel, onReveal, onConfirm, onCancel } =
-	useConfirmDialog()
+const isOpen = ref(false)
+
+function closeModal() {
+	isOpen.value = false
+}
+function openModal() {
+	isOpen.value = true
+}
 </script>
 
 <template>
-	<button @click="reveal" class="text-dark dark:text-white underline">
+	<button @click="openModal" class="text-dark dark:text-white underline">
 		Show me full list
 	</button>
+	<TransitionRoot appear :show="isOpen" as="template">
+		<Dialog as="div" @close="closeModal" class="relative z-10">
+			<TransitionChild
+				as="template"
+				enter="duration-300 ease-out"
+				enter-from="opacity-0"
+				enter-to="opacity-100"
+				leave="duration-200 ease-in"
+				leave-from="opacity-100"
+				leave-to="opacity-0"
+			>
+				<div class="fixed inset-0 bg-black bg-opacity-50" />
+			</TransitionChild>
 
-	<teleport to="body">
-		<div v-if="isRevealed" class="modal-bg">
-			<div class="modal">
-				<h2>Confirm?</h2>
-				<button @click="confirm">Yes</button>
-				<button @click="cancel">Cancel</button>
+			<div class="fixed inset-0 overflow-y-auto">
+				<div
+					class="flex min-h-full items-center justify-center p-4 text-center"
+				>
+					<TransitionChild
+						as="template"
+						enter="duration-300 ease-out"
+						enter-from="opacity-0 scale-95"
+						enter-to="opacity-100 scale-100"
+						leave="duration-100 ease-in"
+						leave-from="opacity-100 scale-100"
+						leave-to="opacity-0 scale-95"
+					>
+						<DialogPanel
+							class="w-full max-w-xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
+						>
+							<DialogTitle class="text-xl flex justify-between">
+								<h3>Full list of skills</h3>
+								<XMarkIcon
+									@click="closeModal"
+									class="w-5 h-5 text-black cursor-pointer"
+								/>
+							</DialogTitle>
+							<div class="mt-2">
+								<p class="text-sm text-gray-500">
+									Your payment has been successfully submitted. We’ve
+									sent you an email with all of the details of your
+									order.
+								</p>
+							</div>
+						</DialogPanel>
+					</TransitionChild>
+				</div>
 			</div>
-		</div>
-	</teleport>
+		</Dialog>
+	</TransitionRoot>
 </template>
