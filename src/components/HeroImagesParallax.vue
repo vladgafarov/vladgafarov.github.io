cf2c6977ce57d16596a944d96bcdf6b41d4821a3
Vlad Gafarov <vlad.gafarov02@gmail.com>
2023-01-29 12:59:48 +0500

    update hero

--- src/components/HeroImagesParallax.vue
+++ src/components/HeroImagesParallax.vue
@@ -4,15 +4,15 @@ import HeroImageItem from './HeroImageItem.vue'
 
 <template>
 	<div
-		class="hidden absolute top-1/2 h-[65%] 2xl:h-[45%] -translate-y-1/2 transform inset-x-[10%] lg:flex justify-between pointer-events-none"
+		class="hidden absolute top-1/2 h-[50%] 2xl:h-[45%] -translate-y-1/2 transform inset-x-[10%] lg:flex justify-between pointer-events-none"
 	>
 		<div class="flex flex-col justify-between">
 			<HeroImageItem :position="-3" image="infinity.png" :rotate="-70" />
 			<HeroImageItem :position="3" image="cube.png" :rotate="15" />
 		</div>
 		<div class="flex flex-col justify-between">
-			<HeroImageItem :position="5" image="react-logo.png" :rotate="-10" />
-			<HeroImageItem :position="-5" image="vue-logo.png" :rotate="-14" />
+			<HeroImageItem :position="1" image="react-logo.png" :rotate="-10" />
+			<HeroImageItem :position="-8" image="vue-logo.png" :rotate="-14" />
 		</div>
 	</div>
 </template>
